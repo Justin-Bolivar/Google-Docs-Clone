935e63db05b4431aa50c1a003f756eec484f9db8
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-04-29 20:32:05 +0800

    Google Sign In Working

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_docs_clone_flutter/colors.dart';
+import 'package:google_docs_clone_flutter/repository/auth_repository.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends ConsumerWidget {
   const LoginPage({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authRepositryProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         body: Center(
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () => signInWithGoogle(ref),
         icon: Image.asset(
           'assets/images/g-logo.png',
           height: 20,
