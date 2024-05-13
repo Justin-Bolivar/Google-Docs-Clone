0540c988757212c991943dc3d9d48a47d7170964
Justin Bolivar <justinantonio.bolivar.21@usjr.edu.ph>
2024-05-13 18:20:39 +0800

    create document error fixed

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const authRouter = require("./routes/auth");
+const documentRouter = require("./routes/document");
 
 
 const PORT = 3001;
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authRouter);
+app.use(documentRouter);
 
 const DB = "mongodb+srv://justinbolivar:rootpassword@docscluster.brudllv.mongodb.net/?retryWrites=true&w=majority&appName=DocsCluster"
 
